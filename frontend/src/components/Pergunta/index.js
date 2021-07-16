a8947080aa1733866f17dc97fabd6d3bf4698d7b
LucasTAlmeida <49998772+LucasTAlmeida@users.noreply.github.com>
2021-07-16 01:44:52 -0300

    Update index.js

--- frontend/src/components/Pergunta/index.js
+++ frontend/src/components/Pergunta/index.js
@@ -1,52 +1,94 @@
-import React, { useContext, useState } from "react";
-import { useHistory } from "react-router-dom";
 
 
-import "./styles.css";
+import React, { useState } from 'react';
 
-import { AuthContext } from '../context/AuthContext';
+import "./pergunta.css";
 
+export default function Pergunta() {
+	const questions = [
+		{
+			questionText: 'Quantos anos tem o Brasil?',
+			answerOptions: [
+				{ answerText: '518', isCorrect: false },
+				{ answerText: '519', isCorrect: false },
+				{ answerText: '520', isCorrect: true },
+				{ answerText: '521', isCorrect: false },
+			],
+		},
+		{
+			questionText: 'Qual destas NÃO é invenção do Tesla?',
+			answerOptions: [
+				{ answerText: 'Motor de indução', isCorrect: false },
+				{ answerText: 'Bomba de calor por absorção', isCorrect: true },
+				{ answerText: 'Controle remoto', isCorrect: false },
+				{ answerText: 'Raio violeta', isCorrect: false },
+			],
+		},
+		{
+			questionText: 'Qual opção NÃO é uma fruta em inglês?',
+			answerOptions: [
+				{ answerText: 'Ginger', isCorrect: true },
+				{ answerText: 'Pear', isCorrect: false },
+				{ answerText: 'Soursop', isCorrect: false },
+				{ answerText: 'Star fruit', isCorrect: false },
+			],
+		},
+		{
+			questionText: 'Quantos trabalhos realizou Hércules segundo a mitologia grega?',
+			answerOptions: [
+				{ answerText: '15', isCorrect: false },
+				{ answerText: '10', isCorrect: false },
+				{ answerText: '7', isCorrect: false },
+				{ answerText: '12', isCorrect: true },
+			],
+		},
+	];
 
-function Pergunta(){
-    const history = useHistory();
-    const { authentication } = useContext(AuthContext);
-    const [resposta, setResposta] = useState({});
+	const [currentQuestion, setCurrentQuestion] = useState(0);
+	const [showScore, setShowScore] = useState(false);
+	const [score, setScore] = useState(0);
 
-    function handleChange(e){
-        //e.target.name
-        setResposta({
-            ...resposta,
-            [e.target.name]: e.target.value,});
-    }
+	const handleAnswerOptionClick = (isCorrect) => {
+		if (isCorrect) {
+			setScore(score + 1);
+		}
 
-    function handleSubmit(e){
-        e.preventDefault();
-        //mandar inf para o back
-        authentication(resposta);
-        // encamminha apos logar para stado / que é Homne
-        history.push("/resultados");
-    }
-
-   return (
-       
-       <div className ="container">
-        <form onSubmit={handleSubmit}>
-            <h2>Pergunta</h2>
-            <h3>Você gosta de responder questionarios?</h3>
-            
-            <label className = "row">
-            <button value="sim" onClick={handleChange}>Sim</button>
-            </label>
-
-            <label className = "row">
-            <button value="não" onClick={handleChange}>Não</button>                  
-            </label>
-
-            <label className = "row">
-                <button value="as vezes" onClick={handleChange}>As vezes</button>
-            </label>
-        </form>
-       </div> 
-   );
+		const nextQuestion = currentQuestion + 1;
+		if (nextQuestion < questions.length) {
+			setCurrentQuestion(nextQuestion);
+		} else {
+			setShowScore(true);
+		}
+	};
+	return (
+      
+		<div className='container3'>
+			{showScore ? (
+				<div className='score-section'>
+					You scored {score} out of {questions.length}
+				</div>
+			) : (
+				<>
+					<div className='question-section'>
+						<div className='question-count'>
+                            
+							<span>Questão {currentQuestion + 1}</span>/{questions.length}
+                           
+                        </div>
+                        
+                        <div className='question-text'>{questions[currentQuestion].questionText}</div>
+                    
+                        <p>Clique Na Resposta</p>
+                    </div>
+                
+					<div className='answer-section'>
+						{questions[currentQuestion].answerOptions.map((answerOption) => (
+							<button onClick={() => handleAnswerOptionClick(answerOption.isCorrect)}>{answerOption.answerText}</button>
+						))}
+					</div>
+                
+				</>
+			)}
+		</div>
+	);
 }
-export default Pergunta;
