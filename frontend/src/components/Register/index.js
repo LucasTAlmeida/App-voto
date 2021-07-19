ab2c4e0b57cb239e09191006847211ff93f612d4
LucasTAlmeida <49998772+LucasTAlmeida@users.noreply.github.com>
2021-07-19 18:19:45 -0300

    Update index.js

--- frontend/src/components/Register/index.js
+++ frontend/src/components/Register/index.js
@@ -31,19 +31,19 @@ function Register(){
             <h2>Cadastro Jogador</h2>
             <div className = "row">
                 <span>Nome</span>
-                <input type ="text" name= "name" onChange={handleChange} />
+                <input type ="text" name= "name" onChange={handleChange} id ="name" required/>
             </div>
             <div className = "row">
                 <span>Email</span>
-                <input type ="text" name="email" onChange={handleChange}/>
+                <input type ="text" name="email" onChange={handleChange} id ="email" required/>
             </div>
             <div className = "row">
                 <span>Username</span>
-                <input type ="text" name="username" onChange={handleChange}/>
+                <input type ="text" name="username" onChange={handleChange} id ="username" required/>
             </div>
             <div className = "row">
                 <span>Password</span>
-                <input type ="password" name="password" onChange={handleChange} />
+                <input type ="password" name="password" onChange={handleChange} id ="password" required />
             </div>
             <div className = "row">
                 <button>Cadastrar</button>
